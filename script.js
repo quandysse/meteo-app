8c8211421fefe7e35acb6f382882978d606e5af9
quandysse <candicebrigaud@orange.fr>
2021-03-06 03:32:50 +0100

    [JS api done]

--- script.js
+++ script.js
@@ -1,64 +1,5 @@
 // METEO APP //
 
-// faire en sorte d'afficher une ville en dur
-// faire en sorte que ce soit dynamique
-
-let city = 'Paris';
-let cities = document.getElementById('cities');
-
-// https://api.openweathermap.org/data/2.5/weather?q=saint-saulve&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric
-// https://api.openweathermap.org/data/2.5/weather?q=paris&appid=3865df021f9d2983383dd2af4fc0bdb9&units=metric
-
-getWeather (city) => {
-
-}
-    
-    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=3865df021f9d2983383dd2af4fc0bdb9&units=metric'; // api key 
-
-    let requestWeather = new XMLHttpRequest();
-    requestWeather.open('GET', url)
-    requestWeather.responseType = 'json'
-    requestWeather.send();
-
-
-    requestWeather.onload = () =>{
-        
-        if (requestWeather.readyState === XMLHttpRequest.DONE){ // -> on verifie si l'etat de notre requête est égal à l'état accomplie (DONE) de notre objet xmlhttpRequest
-            // verifier si le code http est bon
-            if (requestWeather.status === 200  ){ 
-
-                let responseWeatherStocked = requestWeather.response;
-                console.log(responseWeatherStocked);
-
-                let temperature = responseWeatherStocked.main.temp;
-                let city = responseWeatherStocked.name;
-                console.log(temperature);
-                console.log(city);
-                
-                document.getElementById('title').textContent = temperature;
-                document.getElementById('city').textContent = city;
-
-                
-            } else {
-                // si au dessus c pas bon, on previent l'utilisateur qu'il y a une erreur
-                alert('Il y a un prblm, merci de revenir plus tard')
-            }
-        } 
-    }
-
-    cities.addEventListener('Click', (e) =>{
-        newCity = prompt('Rendter une ville')
-
-        getWeather (newCity);
-    })
-
-
-
-// //// BIEN DÉBUTER ////
-// Pour débuter il faut commencer par comprendre ce que fait l'API que nous allons utiliser.
-// Nous allons nous servir de OpenWeatherMap dans ce projet. Url -> https://openweathermap.org/current 
-// Pour celà il vous faut créer un compte et aller récupérer vous clés d'API
-// à cette adresse ->
 // !!!!!!!!!!!!! ATTENTION !!!!!!!!!!!!!
 // DANS UN VÉRITABLE PROJET, VOS CLÉES D'API NE DOIVENT JAMAIS ÊTRE MISE EN DUR
 // DANS VOTRE CODE. NOUS NE NOUS ATTARDERONS SUR LE SUJET DANS CE COURS, MAIS
@@ -67,3 +8,49 @@ getWeather (city) => {
 // BANQUE À UN PIRATAGE ET DES PRÉJUDICE IMPORTANTS. JE CONNAIS DES DÉVELOPPEURS QUI
 // SE SONT FAIT FATURER PLUSIEURS DIXAINES DE MILIERS D'EUROS EN LEUR NOM. 
 // Aller regarder git ignore
+
+const form = document.getElementById('form');
+const city = document.getElementById('city');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e);
+    // cle moi
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city.value + '&appid=3865df021f9d2983383dd2af4fc0bdb9&units=metric';
+
+    // cle emeric
+    // const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city.value + '&appid=ff349be070b028d291296e97b0cc432c&units=metric';
+
+    let requestWeather = new XMLHttpRequest();
+    
+    requestWeather.open('GET', url);
+    requestWeather.responseType = 'json';
+    requestWeather.send();
+    
+    requestWeather.onload = () =>{
+        if (requestWeather.readyState === XMLHttpRequest.DONE){
+            if (requestWeather.status === 200){ 
+                let responseWeatherStocked = requestWeather.response;
+                console.log(responseWeatherStocked);
+                let temperature = responseWeatherStocked.main.temp;                
+                // document.getElementById('title').textContent = temperature;
+                // document.getElementById('city').textContent = city.value + ' : ' + temperature + '°';
+                document.getElementById('temp').textContent = city.value + ' : ' + temperature + '°';
+                // console.log(document.getElementById('temp'))
+
+            } else {
+                alert("Ville non valide");
+            }
+        } 
+    }
+
+})
+
+
+
+
+// //// BIEN DÉBUTER ////
+// Pour débuter il faut commencer par comprendre ce que fait l'API que nous allons utiliser.
+// Nous allons nous servir de OpenWeatherMap dans ce projet. Url -> https://openweathermap.org/current 
+// Pour celà il vous faut créer un compte et aller récupérer vous clés d'API
+// à cette adresse ->
